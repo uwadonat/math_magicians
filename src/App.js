99dfc8e2cc672aa3960e3358336eedd0413341c7
uwadonat <uwadonat@gmail.com>
2021-08-17 17:00:09 +0200

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react';
 import Calculator from './components/Calculator';
-function App() {
-  return (
-    <div className="App">
+
+class App extends React.Component {
+  render() {
+    return (
       <Calculator />
-    </div>
-  );
+    )
+  }
 }
 
 export default App;

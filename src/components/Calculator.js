68a1d2be41b3c5c4526a01ff965c13e10477685b
uwadonat <uwadonat@gmail.com>
2021-08-19 14:53:26 +0200

    Implement the event handlers

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState((state) => calculate(state, e.target.textcontent));
+  }
+
   render() {
+    const { next, total } = this.state;
     return (
       <div className="calc-container">
-        <div className="calc-display">0</div>
+        <div className="calc-display">{ next || total || 0 }</div>
         <button type="button" className="gray-btn">AC</button>
         <button type="button" className="gray-btn">+/-</button>
         <button type="button" className="gray-btn">%</button>

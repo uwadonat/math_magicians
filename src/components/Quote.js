0384280141baf697accd6774bea3f364aecc635d
uwadonat <uwadonat@gmail.com>
2021-08-24 18:46:51 +0200

    Update quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Quote() {
     return(
-        <div>
+        <div className='container quote'>
             <p>
             Mathematics is not about numbers, eqations, computations,
         or algorithms: it is about understanding. -William Paul Thurston

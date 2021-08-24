fbd255de8d49a0ca3edc939e221d568e9221ec73
uwadonat <uwadonat@gmail.com>
2021-08-24 18:12:52 +0200

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <div className='container header'>
+            <h1 className='brand-name'>Math Magicians</h1>
+
+        </div>
+    )
+}
+
+export default Header;

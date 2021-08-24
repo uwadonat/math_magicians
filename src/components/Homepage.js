e28f37e4e72f6ac734a4a90d4cbab72c37f53508
uwadonat <uwadonat@gmail.com>
2021-08-24 17:55:08 +0200

    Add Homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+    return (
+        <div>
+            <h2>Welcome on our Calculator</h2>
+            <p> Here is my page</p>
+        </div>
+    )
+}
+
+export default Homepage;

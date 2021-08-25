c4de0818f6a4ed1df789ee959dbcfbc1b955478d
uwadonat <uwadonat@gmail.com>
2021-08-25 09:38:08 +0200

    Add route and path to pages

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,7 +4,7 @@ function Homepage() {
     return (
         <div>
             <h2>Welcome on our Calculator</h2>
-            <p> Here is my page</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam nobis id, provident reprehenderit, temporibus laudantium aperiam ratione perferendis veritatis expedita consequatur iure error! Deserunt labore harum quidem esse, expedita quibusdam.</p>
         </div>
     )
 }

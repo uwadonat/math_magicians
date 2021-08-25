d14cd16b6dabf5a219f3a4e602fcf5240986f3d5
uwadonat <uwadonat@gmail.com>
2021-08-25 15:02:05 +0200

    Add test for calculator using jest

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,12 @@
+import { isNumber } from "./calculate";
+
+describe('isNumber', () => {
+    test('Shuold return true if the input is a number', () => {
+        expect(isNumber('5')).toBeTruthy();
+    });
+
+    test('Should return false if  the input value is not a number', () => {
+        expect(isNumber('xxx')).toBeFalsy();
+    });
+})
+

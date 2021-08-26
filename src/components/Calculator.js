4d22a4f6a054ef52a7ca64ac4215e985f1d8941f
uwadonat <uwadonat@gmail.com>
2021-08-26 04:58:14 +0200

    last update

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,47 +6,30 @@ function Calculator() {
   const [calcObject, setCalcObject] = useState({});
 
   function handleClick(e) {
-    e.preventDefault();
-    try {
-      setCalcObject({ ...calcObject, ...calculate(calcObject, e.target.textContent) });
-    } catch (error) {
-      return calcObject;
-    }
-    return 0;
+    setCalcObject({ ...calcObject, ...calculate(calcObject, e.target.textContent) });
   }
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="calculate-page">
-        <h2>Let&apos;s do some Math</h2>
-        <div className="calc-container">
-          <div className="calc-display">
-            {next || total || 0}
-          </div>
-          <button type="button" onClick={this.handleClick} className="gray-btn">AC</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">+/-</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">%</button>
-          <button type="button" onClick={this.handleClick} className="orange-btn">÷</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">7</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">8</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">9</button>
-          <button type="button" onClick={this.handleClick} className="orange-btn">x</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">4</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">5</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">6</button>
-          <button type="button" onClick={this.handleClick} className="orange-btn">-</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">1</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">2</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">3</button>
-          <button type="button" onClick={this.handleClick} className="orange-btn">+</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn zero-btn">0</button>
-          <button type="button" onClick={this.handleClick} className="gray-btn">.</button>
-          <button type="button" onClick={this.handleClick} className="orange-btn">=</button>
-        </div>
+  const { next, total } = calcObject;
+
+  const buttonNames = [
+    'AC', '+/-', '%', '÷', '7', '8', '9', 'x',
+    '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',
+  ];
+
+  const btnClass = (i) => ((((i + 1) % 4 === 0) || i === 18) ? 'orange-btn' : 'gray-btn');
+
+  return (
+    <div className="container page-border">
+      <h2>Let&apos;s do some math!</h2>
+      <div className="calc-container">
+        <div className="calc-display">{next || total || 0}</div>
+        {buttonNames.map((name, i) => (
+          <button key={i.toString()} type="button" onClick={handleClick} className={btnClass(i)}>
+            {name}
+          </button>
+        ))}
       </div>
-    );
-  }
-
+    </div>
+  );
 }
 export default Calculator;
